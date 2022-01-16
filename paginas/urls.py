b1236d1da474d2fee378e0a5b9f0a9801592a5ae
Marcelle Sales <lfar05071@gmail.com>
2022-01-16 17:49:22 -0300

    teste

---
+++ paginas/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import PaginaInicial
+from . import views
+
+urlpatterns = [
+    #path('endereço/', minhaview.as_view(), name='nome-da-url'),
+    path('home', PaginaInicial.as_view(), name='index'),
+    path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView, name="task-view"),
+    path('newtask/', views.newTask, name="new-task"),
+    path('edit/<int:id>', views.editTask, name="edit-task"),
+    path('delete/<int:id>', views.deleteTask, name="delete-task"),
+]
